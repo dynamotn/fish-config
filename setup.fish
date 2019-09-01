9f67a0fbaf873e1f87dc150d8b8b50f28754e95c
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-02 01:52:09 +0700

    Basic fish shell with fisher plugin manager

---
+++ setup.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+set UPDATE false
+for i in (echo $argv | sed "s|--*|\n|g" | grep -v '^$')
+  echo $i | read -l option value
+  switch $option
+    case u update
+      set UPDATE true
+  end
+end
+if not functions -q fisher; or eval $UPDATE
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+end
+fish -c fisher
