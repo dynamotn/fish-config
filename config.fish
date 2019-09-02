311c77473a56ced5901f12148dd1f8aca646746d
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-02 12:28:41 +0700

    Add GPG variable

--- config.fish
+++ config.fish
@@ -1,9 +1,15 @@
+## Some environment variables {
 # Path of Fish config
 set -gx CONFIG_PATH (dirname (readlink -f (status --current-filename)))
 
-# Theme config
+# GPG interface to insert passphrase
+set -gx GPG_TTY (tty)
+## }
+
+## Theme config {
 set -g theme_date_format "+%a %H:%M"
 set -g theme_show_exit_status yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme dark
+## }
