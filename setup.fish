86ec9871488333ab17e9a02cbcc0cb91af100cdf
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-02 14:31:37 +0700

    Add fzf as fuzzy finder

--- setup.fish
+++ setup.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 set SETUP_DIR (dirname (readlink -m (status --current-filename)))
 set UPDATE false
+
+# Install fisher
 for i in (echo $argv | sed "s|--*|\n|g" | grep -v '^$')
   echo $i | read -l option value
   switch $option
@@ -11,5 +13,11 @@ end
 if not functions -q fisher; or eval $UPDATE
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 end
-fisher add <$SETUP_DIR/.pkg
-fisher add $SETUP_DIR/dynamo
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+# Install fish package
+fish -c fisher add <$SETUP_DIR/.pkg
+fish -c fisher add $SETUP_DIR/dynamo
