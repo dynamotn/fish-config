9b1b36be1fd15eb5c2e96a764995b53982406e3b
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-02 12:58:15 +0700

    Add DIRCOLORS to show color with ls

--- config.fish
+++ config.fish
@@ -17,4 +17,8 @@ set -g theme_color_scheme dark
 ## Fish or shell miscelaneous config {
 # Enable vi key bindings
 fish_vi_key_bindings
+
+# DIRCOLOR for ls
+alias ls "grc.wrap ls --color=always -C"
+eval (dircolors -c $CONFIG_PATH/DIRCOLORS 2> /dev/null)
 ## }
