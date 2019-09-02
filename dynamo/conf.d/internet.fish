ab3959772df311c02f239530c7640fba3d972a7f
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-02 15:30:18 +0700

    Add some system administrator's commands

--- dynamo/conf.d/internet.fish
+++ dynamo/conf.d/internet.fish
@@ -1,11 +1,3 @@
-function ipinfo
-  if test -z "$argv"
-    curl -s http://ipinfo.io
-  else
-    curl -s http://ipinfo.io/$argv
-  end
-end
-
 function thoitiet
   if test -d $argv
     curl -s wttr.in
---
+++ dynamo/conf.d/sysad.fish
@@ -0,0 +1,28 @@
+# Average load
+function average_load
+  uptime | awk -F'[a-z]: ' '{ print $2 }'
+end
+
+# Rsync
+aliasdy syncdy 'rsync --delete -avhz'
+
+# Ping
+aliasdy shortping 'ping -c 5'
+aliasdy fastping 'ping -c 50 -i0.2'
+
+# Netstat
+aliasdy openports 'netstat -tuplan'
+aliasdy listenports 'netstat -tuplen'
+
+# IP
+function ipwan
+  if test -z "$argv"
+    curl -s http://ipinfo.io
+  else
+    curl -s http://ipinfo.io/$argv
+  end
+end
+
+function iplan
+  hostname -i
+end
