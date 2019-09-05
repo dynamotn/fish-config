eee84fbf3abc386c58329562ca3c607272712c7e
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-05 12:25:02 +0700

    Change theme of PS1

--- config.fish
+++ config.fish
@@ -34,7 +34,7 @@ set -g theme_date_format "+%a %H:%M"
 set -g theme_show_exit_status yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme dark
+set -g theme_color_scheme terminal
 ## }
 
 ## Fish or shell miscelaneous config {
