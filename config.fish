2732921e35a64aa0b2a95037916fbd72120da8eb
Trần Đức Nam <dynamo.foss@gmail.com>
2020-11-17 14:54:37 +0700

    Show kube context on prompt

--- config.fish
+++ config.fish
@@ -56,6 +56,7 @@ set -g theme_show_exit_status yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme terminal
+set -g theme_display_k8s_context yes
 ## }
 
 ## Fish or shell miscelaneous config {
