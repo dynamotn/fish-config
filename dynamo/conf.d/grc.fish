ea274a6dcdc3348b9ae6a0c70899580b60260786
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-21 02:05:29 +0700

    Update grc list command

--- dynamo/conf.d/grc.fish
+++ dynamo/conf.d/grc.fish
@@ -1,21 +1,5 @@
-if type -q grc
-  set -l execs cat cvs df diff dig gcc g++ ls ifconfig \
-       make mount mtr netstat ping ps tail traceroute \
-       wdiff blkid du dnf docker docker-machine env id ip iostat \
-       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
-       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount tune2fs \
-       tcpdump tune2fs \
-       vmstat w who
-
-  if set -q grc_plugin_execs
-    set execs $grc_plugin_execs
-  end
-
-  for executable in $execs
-    if type -q $executable
-      function $executable --inherit-variable executable --wraps=$executable
-        grc.wrap $executable $argv
-      end
-    end
+function grc_initial -e fish_prompt
+  if type -q grc
+    grc_setup
   end
 end
