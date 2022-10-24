f17c3dae65ea08c6dcbaf4dd155500553ec94f4b
Trần Đức Nam <dynamo.foss@gmail.com>
2022-10-25 06:36:28 +0700

    feat: install up-to-date version of tools

--- dynamo/conf.d/software.fish
+++ dynamo/conf.d/software.fish
@@ -1,19 +1,27 @@
 function install_gcloud
-  if type -q gcloud; or test -q asdf
+  type -q gcloud; or update_gcloud
+end
+
+function update_gcloud
+  if not type -q asdf
     return
   end
-
   asdf plugin-add gcloud
   asdf install gcloud latest
+  asdf global gcloud (asdf latest gcloud)
 end
 
 function install_awscli
-  if type -q awscli; or test -q asdf
+  type -q awscli; or update_awscli
+end
+
+function update_awscli
+  if not type -q asdf
     return
   end
-
   asdf plugin-add awscli
   asdf install awscli latest
+  asdf global awscli (asdf latest awscli)
 end
 
 function install_asdf
@@ -21,35 +29,58 @@ function install_asdf
     return
   end
 
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
   mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 end
 
+function update_asdf
+  if not type -q asdf
+    install_asdf
+  end
+
+  asdf update
+end
+
 function install_kubectl
-  if type -q kubectl; or test -q asdf
+  type -q kubectl; or update_kubectl
+end
+
+function update_kubectl
+  if not type -q asdf
     return
   end
 
   asdf plugin-add kubectl
   asdf install kubectl latest
+  asdf global kubectl (asdf latest kubectl)
 end
 
 function install_helm
-  if type -q helm; or test -q asdf
+  type -q helm; or update_helm
+end
+
+function update_helm
+  if not type -q asdf
     return
   end
 
   asdf plugin-add helm
   asdf install helm latest
+  asdf global helm (asdf latest helm)
 end
 
 function install_terraform
-  if type -q terraform; or test -q asdf
+  type -q terraform; or update_terraform
+end
+
+function update_terraform
+  if not type -q asdf
     return
   end
 
   asdf plugin-add terraform
   asdf install terraform latest
+  asdf global terraform (asdf latest terraform)
 end
 
 function install_fzf
@@ -58,6 +89,12 @@ function install_fzf
   end
 
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --no-zsh --all
+end
+
+function update_fzf
+  cd ~/.fzf
+  git pull origin master
   ~/.fzf/install --no-zsh --all
 end
 
@@ -72,10 +109,16 @@ function install_nix
 end
 
 function install_pre_commit
-  if type -q pre-commit
+  type -q pre-commit; or update_pre_commit
+end
+
+function update_pre_commit
+  if not type -q asdf
     return
   end
 
-  type -q pip3; and pip3 install pre-commit
-  type -q asdf; and asdf reshim python
+  asdf global python system
+
+  pip3 install --user pre-commit
+  asdf reshim python
 end
